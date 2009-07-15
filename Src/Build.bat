ecded685a071e1631882efdd83ae26882eeb572d
DelphiDabbler <git@delphidabbler.com>
2009-07-15 15:40:28 +0000

    Added Mozilla Public License / GPL 2.0 / LGPL 2.1 tri-license.

--- Src/Build.bat
+++ Src/Build.bat
@@ -1,31 +1,70 @@
-@rem ---------------------------------------------------------------------------
 @rem Build.bat
 @rem
-@rem Script used to build the DelphiDabbler CompFileDate project
-@rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2009
+@rem Script used to build the DelphiDabbler CompFileDate project.
 @rem
 @rem $Rev$
 @rem $Date$
 @rem
 @rem Requires:
-@rem   Borland Delphi 2006
-@rem   Borland BRCC32 from Delphi 2006 installation
-@rem   DelphiDabbler Version Information Editor v2.11.2
+@rem    Borland Delphi 2006
+@rem    Borland BRCC32 from Delphi 2006 installation
+@rem    DelphiDabbler Version Information Editor v2.11.2
 @rem
 @rem Also requires the following environment variables:
-@rem   DELPHI2006 to be set to the install directory of Delphi 2006
+@rem    DELPHI2006 to be set to the install directory of Delphi 2006
 @rem
-@rem Switches: exactly one of the following must be provided
-@rem   all - build everything
-@rem   res - build binary resource files only
-@rem   pas - build Delphi Pascal project only
+@rem  Switches: exactly one of the following must be provided
+@rem    all - build everything
+@rem    res - build binary resource files only
+@rem    pas - build Delphi Pascal project only
 @rem
-@rem ---------------------------------------------------------------------------
+@rem ***** BEGIN LICENSE BLOCK *****
+@rem
+@rem Version: MPL 1.1/GPL 2.0/LGPL 2.1
+@rem
+@rem The contents of this file are subject to the Mozilla Public License
+@rem Version 1.1 (the "License")@rem * you may not use this file except in
+@rem compliance with the License. You may obtain a copy of the License at
+@rem http://www.mozilla.org/MPL/MPL-1.1
+@rem
+@rem Software distributed under the License is distributed on an "AS IS"
+@rem basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+@rem License for the specific language governing rights and limitations under
+@rem the License.
+@rem
+@rem The Original Code is Build.bat from the CompFileDate project.
+@rem
+@rem The Initial Developer of the Original Code is Peter Johnson
+@rem (http://www.delphidabbler.com/).
+@rem
+@rem Portions created by the Initial Developer are Copyright (C) 2009 by
+@rem the Initial Developer. All Rights Reserved.
+@rem
+@rem Contributors:
+@rem    None.
+@rem
+@rem Alternatively, the contents of this file may be used under the terms of
+@rem either the GNU General Public License Version 2 or later (the "GPL"), or
+@rem the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+@rem in which case the provisions of the GPL or the LGPL are applicable
+@rem instead of those above. If you wish to allow use of your version of this
+@rem file only under the terms of either the GPL or the LGPL, and not to allow
+@rem others to use your version of this file under the terms of the MPL,
+@rem indicate your decision by deleting the provisions above and replace them
+@rem with the notice and other provisions required by the LGPL or the GPL. If
+@rem you do not delete the provisions above, a recipient may use your version
+@rem of this file under the terms of any one of the MPL, the GPL or the LGPL.
+@rem
+@rem ***** END LICENSE BLOCK *****
+
+
+rem ----------------------------------------------------------------------------
+rem Initialise
+rem ----------------------------------------------------------------------------
 
 @echo off
-
 setlocal
+
 
 rem ----------------------------------------------------------------------------
 rem Sign on
@@ -38,20 +77,20 @@ echo ---------------------------------------
 
 goto Config
 
+
 rem ----------------------------------------------------------------------------
 rem Configure script per command line parameter
 rem ----------------------------------------------------------------------------
 
 :Config
 echo Configuring script
-rem reset all config variables
 
+rem Reset all config variables
 set BuildAll=
 set BuildResources=
 set BuildPascal=
 
-rem check switch
-
+rem Sheck switch
 if "%~1" == "all" goto Config_BuildAll
 if "%~1" == "res" goto Config_BuildResources
 if "%~1" == "pas" goto Config_BuildPascal
@@ -59,7 +98,7 @@ set ErrorMsg=Unknown switch "%~1"
 if "%~1" == "" set ErrorMsg=No switch specified
 goto Error
 
-rem set config variables
+rem Set config variables
 
 :Config_BuildAll
 set BuildResources=1
@@ -73,9 +112,6 @@ goto Config_OK
 :Config_BuildPascal
 set BuildPascal=1
 goto Config_OK
-
-
-rem script configured OK
 
 :Config_OK
 echo Done.
@@ -93,8 +129,6 @@ if not defined DELPHI2006 goto BadDELPHI2006Env
 echo Done.
 echo.
 goto SetEnvVars
-
-rem we have at least one undefined env variable
 
 :BadDELPHI2002006Env
 set ErrorMsg=DELPHI2006 Environment variable not defined
@@ -150,27 +184,28 @@ if not defined BuildResources goto Build_Pascal
 echo Building Resources
 echo.
 
-rem set required env vars
+rem Set required env vars
 
 rem Ver info resource
 set VerInfoBase=VerInfo
 set VerInfoSrc=%SrcDir%%VerInfoBase%.vi
 set VerInfoTmp=%SrcDir%%VerInfoBase%.rc
 set VerInfoRes=%BinDir%%VerInfoBase%.res
+
 rem Main resource
 set ResourcesBase=Resources
 set ResourcesSrc=%SrcDir%%ResourcesBase%.rc
 set ResourcesRes=%BinDir%%ResourcesBase%.res
 
 rem Compile version information resource
+rem VIedExe creates temp resource .rc file from .vi file
+rem BRCC32Exe compiles temp resource .rc file to required .res
 
 echo Compiling %VerInfoSrc% to %VerInfoRes%
-rem VIedExe creates temp resource .rc file from .vi file
 set ErrorMsg=
 %VIEdExe% -makerc %VerInfoSrc%
 if errorlevel 1 set ErrorMsg=Failed to compile %VerInfoSrc%
 if not "%ErrorMsg%"=="" goto VerInfoRes_End
-rem BRCC32Exe compiles temp resource .rc file to required .res
 %BRCC32Exe% %VerInfoTmp% -fo%VerInfoRes%
 if errorlevel 1 set ErrorMsg=Failed to compile %VerInfoTmp%
 if not "%ErrorMsg%"=="" goto VerInfoRes_End
@@ -257,8 +292,8 @@ rem ----------------------------------------------------------------------------
 rem Finished
 rem ----------------------------------------------------------------------------
 
-
 :End
 echo.
 echo DONE.
 endlocal
+
