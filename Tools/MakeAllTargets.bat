70f56af79cc082013ec7ef0b234cfb3d5093d8a8
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2026-02-03 19:52:59 +0000

    Rationalise use of env vars in MakeAllTargets.bat
    Removed code that detected presence of VIEdRoot env var and that
    reported an error if wasn't defined - VIEdRoot is not directly required
    by the script.
    
    Added a check that the BDS env var is defined & report an error if not -
    this is a basic check that Delphi's rsvars.bat has been called.
    
    Description of how to use script in header comments was re-written.

--- Tools/MakeAllTargets.bat
+++ Tools/MakeAllTargets.bat
@@ -1,22 +1,34 @@
 :: Helper script to build CompFileDate for all target platforms.
 ::
-:: This script compiles DEBUG versions of the Windows 64 and 32 bit builds and
-:: Linux 64 bit build of CompFileDate into the directories specified in project
-:: options.
+:: This script compiles DEBUG versions of CompFileDate for the following target
+:: platforms:
 ::
-:: This script uses MSBuild. The MSBuild project also requires DelphiDabbler
-:: Version Information Editor to be installed.
+::   * Windows 64 bit
+::   * Windows 32 bit
+::   * Linux 64 bit
+:: 
+:: Delphi's version of MSBuild is used to create the builds.
 ::
-:: Get Version Information Editor from https://delphidabbler.com/software/vied
+:: **** Before using this script make sure that the program can be compiled from
+::      within the IDE. Some configuration is required to achieve this. See the
+::      "Step 2: Configure the Build Environment" section of Build.txt for
+::      details.
 ::
-:: To use the script:
-::    1) Start the Embarcadero RAD Studio Command Prompt to set the required
-::       environment variables for MSBuild.
-::    2) Set the BDSBIN variable to %BDS%\bin (required by MSBuild/Delphi).
-::    4) Set the VIEDROOT environment variable to the directory where VIEd.exe 
-::       is installed.
-::    5) Change directory to that where this script is located.
-::    6) Run the script, without parameters
+:: The script must be called from a terminal with a correctly configured
+:: environment. Proceed as follows:
+::
+::   1) Open and configure the terminal window. There are two ways to do this:
+::
+::      a) Start the Embarcadero RAD Studio Command Prompt from the Windows
+::         Start menu. This opens a terminal window with the required
+::         environment variables set.
+::
+::      b) Open a new terminal window. CD to the directory where RAD Studio is
+::         installed. Now CD into the Bin sub-directory. Run rsvars.bat.
+::
+::   2) In the terminal window CD to the directory where this script is located.
+::
+::   3) Run the script, without parameters.
 
 @echo off
 
@@ -24,16 +36,19 @@ echo ----------------------------------
 echo Creating CompFileDate Debug Builds
 echo ----------------------------------
 
-:: Check for required environment variables
-if "%VIEdRoot%"=="" goto envvarerror
+:: Check that required MSBuild environment has been set up
+
+if "%BDS%"=="" goto missing_bds_env_var_error
 
 :: Set variables
+
 set SrcDir=..\Src
 set ProjFileName=CompFileDate.dproj
 
 setlocal
 
 :: Build Pascal
+
 cd %SrcDir%
 
 echo.
@@ -61,11 +76,17 @@ goto end
 
 endlocal
 
-:envvarerror
+:: Error messages
+
+:missing_bds_env_var_error
+
 echo.
-echo ***ERROR: VIEdRoot environment variable not set
+echo *** ERROR: BDS environment variable is not set.
+echo ***        Run rsvars.bat from the bin subdirectory of the Delphi install
+echo ***        directory.
 echo.
 goto end
 
-:: End
+:: Done
+
 :end
